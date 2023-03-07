2826928ca24c133275d236ebc0844f954b8f19cb
Florencia5 <floordarosa2002@gmail.com>
2023-03-07 13:08:35 -0300

    se agrega el proyecto

---
+++ RegistroAuto.ts
@@ -0,0 +1,37 @@
+[
+{
+"modelo": "etios",
+"marca": "toyota",
+"anio": 2022,
+"color": "blanco",
+"kilometraje": 10300,
+},
+ 
+{
+
+"modelo": "argo",
+"marca": "fiat",
+"anio": 2000,
+"color": "negro",
+"kilometraje": 390300,
+},
+{
+    "modelo": "amarok",
+    "marca": "volkswagen",
+    "anio": 2021,
+    "color": "blanco",
+    "kilometraje": 40300,
+},
+{
+    "modelo": "renegade",
+    "marca": "jeep",
+    "anio": 2002,
+    "color": "azul",
+    "kilometraje": 109900,
+}
+
+
+
+
+
+]
